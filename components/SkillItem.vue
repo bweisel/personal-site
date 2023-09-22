9bc3baa931089df400407ba32dd4d25664885bfd
Ben Weisel <bweisel@gmail.com>
2023-09-22 16:20:29 -0600

    First commit

---
+++ components/SkillItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>{{ skill.name }}</div>
+    <div>{{ skill.description }}</div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  skill: {
+    type: Object,
+    required: true,
+  },
+});
+const { skill } = props;
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>Hi</div>
+    <div>
+      <SkillItem v-for="skill in skills" :key="skill.name" :skill="skill" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const skills = ref([{ name: "SQL", description: "Database Query Language" }]);
+</script>
