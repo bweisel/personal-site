186ec22e07562561b6e58d3ef0b72db62f0f08d5
Ben Weisel <bweisel@gmail.com>
2023-09-26 16:55:35 -0600

    pre render homepage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  routeRules: {
+    // Homepage pre-rendered at build time
+    "/": { prerender: true },
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
