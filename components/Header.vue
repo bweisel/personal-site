b9eca20a9adc9d043d2adc62c5f9bc4e88a3430d
Ben Weisel <bweisel@gmail.com>
2023-09-26 16:01:15 -0600

    Add in header

---
+++ components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="block mb-2 md:flex">
+    <div class="w-full max-w-150">
+      <img
+        class="rounded-full transform transition-all duration-150 hover:scale-105"
+        :src="profile.image"
+        :alt="profile.name"
+      />
+    </div>
+    <div class="flex-none pt-6 md:pt-1 md:flex-1 md:pl-20">
+      <h1
+        class="text-5xl text-gray-900 font-bold leading-tight hover:text-black"
+      >
+        {{ profile.name }}
+      </h1>
+      <p class="text-gray-600 mt-4">
+        {{ profile.subLine }}
+      </p>
+      <ul class="mt-4 uppercase tracking-wider">
+        <li class="inline list-none pr-4">
+          <NuxtLink
+            class="inline-block py-2 font-semibold text-xs text-gray-600 hover:text-black"
+            :to="profile.github"
+          >
+            GitHub
+          </NuxtLink>
+        </li>
+        <li class="inline list-none pr-4">
+          <NuxtLink
+            class="inline-block py-2 font-semibold text-xs text-gray-600 hover:text-black"
+            :to="profile.linkedin"
+          >
+            LinkedIn
+          </NuxtLink>
+        </li>
+        <li class="inline list-none pr-4">
+          <NuxtLink
+            class="inline-block py-2 font-semibold text-xs text-gray-600 hover:text-black"
+            :to="profile.twitter"
+          >
+            Twitter
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  profile: {
+    type: Object,
+    required: true,
+  },
+});
+const { profile } = props;
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="relative">
     <!-- Page Wrapper-->
     <div class="p-8 relative max-w-screen-xl xs:p-24">
+      <Header :profile="profile"></Header>
       <!-- About Me-->
       <Section :title="'About Me'">
         <div class="mb-6">
@@ -27,6 +28,16 @@
 </template>
 
 <script setup>
+const profile = ref({
+  name: "Ben Weisel",
+  image: "/headshot.png",
+  subLine:
+    "Principal software engineer and leader with an expertise in developing advanved data-driven applications of all shapes and sizes.",
+  twitter: "https://twitter.com/bweisel",
+  github: "https://github.com/bweisel",
+  linkedin: "https://www.linkedin.com/in/benweisel/",
+});
+
 const about = ref(
   "Currently building advanced NBA Analytics for Cleaning the Glass. Based in Denver, CO."
 );
