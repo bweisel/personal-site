ad7094842c03b5928305a94705aebc762e459dfa
Ben Weisel <bweisel@gmail.com>
2023-09-26 21:55:19 -0600

    Add About component

---
+++ components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mb-6">
+    <p>{{ about }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  about: {
+    type: String,
+    required: true,
+  },
+});
+const { about } = props;
+</script>
