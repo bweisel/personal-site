abc92faa738accfa9fd9699d8ea6ef999eff4223
Ben Weisel <bweisel@gmail.com>
2023-09-26 21:55:09 -0600

    Add ProjectItem component

---
+++ components/ProjectItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mb-6">
+    <h3 class="font-semibold text-gray-900 pb-1">
+      <NuxtLink
+        :to="project.link"
+        class="hover:underline hover:text-black"
+        target="_blank"
+      >
+        {{ project.name }}
+      </NuxtLink>
+    </h3>
+    <p class="text-md text-gray-600 font-light">{{ project.description }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  project: {
+    type: Object,
+    required: true,
+  },
+});
+const { project } = props;
+</script>
